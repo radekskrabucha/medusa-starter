d70881b35eab80384821081886d81ab33ed97c0a
radekskrabucha <rskrabucha13@gmail.com>
2025-02-19 16:27:44 -0500

    chore: format

--- apps/medusa/medusa-config.ts
+++ apps/medusa/medusa-config.ts
@@ -9,8 +9,8 @@ module.exports = defineConfig({
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret'
     }
   }
 })
