4872bcd18d44d77ce0ff475da5101d2443bc1c32
radekskrabucha <rskrabucha13@gmail.com>
2025-02-27 08:39:50 -0500

    fix(web): remove default SEO title and description

--- apps/web/app/routes/__root.tsx
+++ apps/web/app/routes/__root.tsx
@@ -40,11 +40,7 @@ export const Route = createRootRoute({
         name: 'viewport',
         content: 'width=device-width, initial-scale=1'
       },
-      ...seo({
-        title:
-          'TanStack Start | Type-Safe, Client-First, Full-Stack React Framework',
-        description: `TanStack Start is a type-safe, client-first, full-stack React framework. `
-      })
+      ...seo({ title: undefined })
     ],
     links: [
       { rel: 'stylesheet', href: appCss },
