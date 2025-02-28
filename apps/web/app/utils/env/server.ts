9b72169de319da606821a1e443ecd3c1c18919b5
radekskrabucha <rskrabucha13@gmail.com>
2025-02-28 18:09:16 -0500

    feat(web): add NODE_ENV to server environment configuration

--- apps/web/app/utils/env/server.ts
+++ apps/web/app/utils/env/server.ts
@@ -3,6 +3,9 @@ import { z } from 'zod'
 
 export const envServer = createEnv({
   server: {
+    NODE_ENV: z
+      .enum(['development', 'production', 'test'])
+      .default('development'),
     MEDUSA_BACKEND_URL: z.string().url(),
     MEDUSA_PUBLISHABLE_KEY: z.string().min(1)
   },
