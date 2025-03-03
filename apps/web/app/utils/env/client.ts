f160c2ae4ef4bb4096bf778d51516634858cdd38
radekskrabucha <rskrabucha13@gmail.com>
2025-03-03 14:13:12 -0500

    refactor(web-env): remove unused test environment variable

--- apps/web/app/utils/env/client.ts
+++ apps/web/app/utils/env/client.ts
@@ -4,7 +4,6 @@ import { z } from 'zod'
 export const envClient = createEnv({
   clientPrefix: 'VITE_',
   client: {
-    VITE_TEST: z.string().min(1),
     VITE_MEDUSA_BACKEND_URL: z.string().url(),
     VITE_MEDUSA_PUBLISHABLE_KEY: z.string().min(1),
     VITE_IS_DEV: z
