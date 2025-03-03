33857ca4f77331cdf4cfbaef89e5669e7b1982e8
radekskrabucha <rskrabucha13@gmail.com>
2025-03-03 15:03:17 -0500

    feat(utils): add retrieve methods for products, categories, and collections

--- packages/utils/src/medusa-actions.ts
+++ packages/utils/src/medusa-actions.ts
@@ -22,7 +22,17 @@ export const getMedusaClientStoreActions = (
     },
     store: {
       listProducts: (params: ListProductsParams) =>
-        client.store.product.list(params)
+        client.store.product.list(params),
+      getProduct: (params: GetProductParams) =>
+        client.store.product.retrieve(params.id, params.fields),
+      listCategories: (params: ListCategoriesParams) =>
+        client.store.category.list(params),
+      getCategory: (params: GetCategoryParams) =>
+        client.store.category.retrieve(params.id, params.fields),
+      listCollections: (params: ListCollectionsParams) =>
+        client.store.collection.list(params),
+      getCollection: (params: GetCollectionParams) =>
+        client.store.collection.retrieve(params.id, params.fields)
     }
   }
 
@@ -52,6 +62,11 @@ export type ClientActions = {
   }
   store: {
     listProducts: (params: ListProductsParams) => ListProductsResponse
+    getProduct: (params: GetProductParams) => GetProductResponse
+    listCategories: (params: ListCategoriesParams) => ListCategoriesResponse
+    getCategory: (params: GetCategoryParams) => GetCategoryResponse
+    listCollections: (params: ListCollectionsParams) => ListCollectionsResponse
+    getCollection: (params: GetCollectionParams) => GetCollectionResponse
   }
 }
 
@@ -74,9 +89,47 @@ export type ResetPasswordParams = Parameters<ResetPassword>[2]
 export type ResetPasswordResponse = ReturnType<ResetPassword>
 
 type SetNewPassword = MedusaClient['auth']['updateProvider']
-export type SetNewPasswordParams = { password: string; token: string }
+export type SetNewPasswordParams = {
+  password: string
+  token: string
+}
 export type SetNewPasswordResponse = ReturnType<SetNewPassword>
 
 type ListProduct = MedusaClient['store']['product']['list']
 export type ListProductsParams = Parameters<ListProduct>[0]
 export type ListProductsResponse = ReturnType<ListProduct>
+
+type GetProduct = MedusaClient['store']['product']['retrieve']
+type GetProductId = Parameters<GetProduct>[0]
+type GetProductFields = Parameters<GetProduct>[1]
+export type GetProductParams = {
+  id: GetProductId
+  fields: GetProductFields
+}
+export type GetProductResponse = ReturnType<GetProduct>
+
+type ListCategories = MedusaClient['store']['category']['list']
+export type ListCategoriesParams = Parameters<ListCategories>[0]
+export type ListCategoriesResponse = ReturnType<ListCategories>
+
+type GetCategory = MedusaClient['store']['category']['retrieve']
+type GetCategoryId = Parameters<GetCategory>[0]
+type GetCategoryFields = Parameters<GetCategory>[1]
+export type GetCategoryParams = {
+  id: GetCategoryId
+  fields: GetCategoryFields
+}
+export type GetCategoryResponse = ReturnType<GetCategory>
+
+type ListCollections = MedusaClient['store']['collection']['list']
+export type ListCollectionsParams = Parameters<ListCollections>[0]
+export type ListCollectionsResponse = ReturnType<ListCollections>
+
+type GetCollection = MedusaClient['store']['collection']['retrieve']
+type GetCollectionId = Parameters<GetCollection>[0]
+type GetCollectionFields = Parameters<GetCollection>[1]
+export type GetCollectionParams = {
+  id: GetCollectionId
+  fields: GetCollectionFields
+}
+export type GetCollectionResponse = ReturnType<GetCollection>
