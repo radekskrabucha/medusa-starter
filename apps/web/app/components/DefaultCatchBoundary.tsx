f9f2328b521cdbad893813e8cae5dd14e6e0b266
radekskrabucha <rskrabucha13@gmail.com>
2025-03-03 20:22:15 -0500

    feat(web): added DefaultErrorFallback with error details, refactored DefaultCatchBoundary to use DefaultErrorFallback components

--- apps/web/app/components/DefaultCatchBoundary.tsx
+++ apps/web/app/components/DefaultCatchBoundary.tsx
@@ -1,25 +1,18 @@
-import { Button, buttonVariants } from '@medusa-starter/ui/button'
-import {
-  ErrorComponent,
-  Link,
-  rootRouteId,
-  useMatch,
-  useRouter
-} from '@tanstack/react-router'
+import { Button } from '@medusa-starter/ui/button'
+import { useRouter } from '@tanstack/react-router'
 import type { ErrorComponentProps } from '@tanstack/react-router'
+import {
+  DefaultErrorFallback,
+  NavigateToHomeButton
+} from './DefaultErrorFallback'
 
 export const DefaultCatchBoundary: React.FC<ErrorComponentProps> = ({
   error
 }) => {
   const router = useRouter()
-  const isRoot = useMatch({
-    strict: false,
-    select: state => state.id === rootRouteId
-  })
 
   return (
-    <div className="flex min-w-0 flex-1 flex-col items-center justify-center gap-6 p-4">
-      <ErrorComponent error={error} />
+    <DefaultErrorFallback error={error}>
       <div className="flex flex-wrap items-center gap-4">
         <Button
           onClick={() => {
@@ -28,26 +21,8 @@ export const DefaultCatchBoundary: React.FC<ErrorComponentProps> = ({
         >
           Try Again
         </Button>
-        {isRoot ? (
-          <Link
-            to="/"
-            className={buttonVariants({ variant: 'secondary' })}
-          >
-            Home
-          </Link>
-        ) : (
-          <Link
-            to="/"
-            className={buttonVariants({ variant: 'secondary' })}
-            onClick={e => {
-              e.preventDefault()
-              window.history.back()
-            }}
-          >
-            Go Back
-          </Link>
-        )}
+        <NavigateToHomeButton />
       </div>
-    </div>
+    </DefaultErrorFallback>
   )
 }
