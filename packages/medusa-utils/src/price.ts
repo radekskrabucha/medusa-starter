05d37737f1355dd5d8d01b479a1303dc795dba14
radekskrabucha <rskrabucha13@gmail.com>
2025-03-05 11:16:51 -0500

    feat(medusa-utils): enhance price utilities with percentage diff and variant price formatting functions

--- packages/medusa-utils/src/price.ts
+++ packages/medusa-utils/src/price.ts
@@ -1,6 +1,9 @@
+import { nonNullable } from '@medusa-starter/utils/common'
+import type { ProductVariant } from './models'
+
 type ConvertToLocaleParams = {
   amount: number
-  currency_code: string
+  currencyCode: string
   minimumFractionDigits?: number
   maximumFractionDigits?: number
   locale?: string
@@ -8,17 +11,99 @@ type ConvertToLocaleParams = {
 
 export const convertToLocale = ({
   amount,
-  currency_code,
+  currencyCode,
   minimumFractionDigits,
   maximumFractionDigits,
   locale = 'en-US'
 }: ConvertToLocaleParams) => {
-  return currency_code
+  return currencyCode
     ? new Intl.NumberFormat(locale, {
         style: 'currency',
-        currency: currency_code,
+        currency: currencyCode,
         minimumFractionDigits,
         maximumFractionDigits
       }).format(amount)
     : amount.toString()
+}
+
+type GetPercentageDiffParams = {
+  original: number
+  calculated: number
+}
+
+export const getPercentageDiff = ({
+  calculated,
+  original
+}: GetPercentageDiffParams) => {
+  const diff = original - calculated
+  const decrease = (diff / original) * 100
+
+  return decrease
+}
+
+type FormatVariantCalculatedPriceParams = Omit<
+  ConvertToLocaleParams,
+  'amount' | 'currency_code'
+> & {
+  currency_code?: string
+}
+
+const createPriceFormatter = (
+  calculatedAmount: number,
+  currencyCode: string | null
+) => {
+  return (params: FormatVariantCalculatedPriceParams) =>
+    convertToLocale({
+      amount: calculatedAmount,
+      currencyCode: params.currency_code ?? currencyCode ?? '',
+      locale: params.locale,
+      minimumFractionDigits: params.minimumFractionDigits,
+      maximumFractionDigits: params.maximumFractionDigits
+    })
+}
+
+export const getVariantPrices = (variant: ProductVariant) => {
+  if (!nonNullable(variant.calculated_price)) {
+    return null
+  }
+  if (!nonNullable(variant.calculated_price.calculated_amount)) {
+    return null
+  }
+  const calculatedPrice = variant.calculated_price
+  const calculatedAmount = variant.calculated_price.calculated_amount
+
+  return {
+    calculatedPrice: {
+      number: calculatedAmount,
+      formatted: createPriceFormatter(
+        calculatedAmount,
+        calculatedPrice.currency_code
+      )
+    },
+    originalPrice: nonNullable(calculatedPrice.original_amount)
+      ? {
+          number: calculatedPrice.original_amount,
+          formatted: createPriceFormatter(
+            calculatedPrice.original_amount,
+            calculatedPrice.currency_code
+          )
+        }
+      : null,
+    currencyCode: calculatedPrice.currency_code,
+    priceType: calculatedPrice.calculated_price?.price_list_type,
+    percentageDiff: () => {
+      if (nonNullable(calculatedPrice.original_amount)) {
+        const diff = getPercentageDiff({
+          original: calculatedPrice.original_amount,
+          calculated: calculatedAmount
+        })
+        return {
+          number: diff,
+          formatted: diff.toFixed(2)
+        }
+      }
+
+      return null
+    }
+  }
 }
