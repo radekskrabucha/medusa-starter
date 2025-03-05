04bf5b3c4e37dcd713ded720f063b3a07e721a12
radekskrabucha <rskrabucha13@gmail.com>
2025-03-05 12:05:16 -0500

    feat(router): add query cache and stale time settings to router configuration

--- apps/web/app/router.tsx
+++ apps/web/app/router.tsx
@@ -1,6 +1,10 @@
 import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routerWithQueryClient } from '@tanstack/react-router-with-query'
-import { getQueryClient } from '~web/lib/reactQuery'
+import {
+  getQueryClient,
+  QUERY_CACHE_TIME,
+  QUERY_STALE_TIME
+} from '~web/lib/reactQuery'
 import { DefaultCatchBoundary } from './components/DefaultCatchBoundary'
 import { NotFound } from './components/NotFound'
 import { routeTree } from './routeTree.gen'
@@ -13,7 +17,11 @@ export function createRouter() {
     context: { queryClient },
     defaultPreload: 'intent',
     defaultErrorComponent: DefaultCatchBoundary,
-    defaultNotFoundComponent: () => <NotFound />
+    defaultNotFoundComponent: () => <NotFound />,
+    defaultGcTime: QUERY_CACHE_TIME,
+    defaultPreloadGcTime: QUERY_CACHE_TIME,
+    defaultStaleTime: QUERY_STALE_TIME,
+    defaultPreloadStaleTime: QUERY_STALE_TIME
   })
 
   return routerWithQueryClient(router, queryClient)
