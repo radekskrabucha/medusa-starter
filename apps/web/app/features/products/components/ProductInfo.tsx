b13f1662ba4456f9711bc447fac34413fd836191
radekskrabucha <rskrabucha13@gmail.com>
2025-03-05 12:15:57 -0500

    feat(ProductInfo): implement variant price calculation and display in product info component

--- apps/web/app/features/products/components/ProductInfo.tsx
+++ apps/web/app/features/products/components/ProductInfo.tsx
@@ -1,4 +1,6 @@
 import type { Product } from '@medusa-starter/medusa-utils/models'
+import { getVariantPrices } from '@medusa-starter/medusa-utils/price'
+import { nonNullable } from '@medusa-starter/utils/common'
 import type React from 'react'
 
 type ProductInfoProps = {
@@ -6,18 +8,22 @@ type ProductInfoProps = {
 }
 
 export const ProductInfo: React.FC<ProductInfoProps> = ({ product }) => {
-  // TODO - Add logic for calculating variant price
-  const variantPrice = 1230.12
-  const formattedPrice = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  }).format(variantPrice)
+  const selectedVariant = product?.variants?.[0]
+
+  // TODO: temporary, should be inferred from the selected options
+  const variantPrice = nonNullable(selectedVariant)
+    ? getVariantPrices(selectedVariant)
+    : null
 
   return (
     <div className="flex flex-col gap-6">
       <div>
         <h1 className="text-3xl font-bold tracking-tight">{product.title}</h1>
-        <p className="mt-4 text-xl font-medium">{formattedPrice}</p>
+        {variantPrice && (
+          <p className="mt-4 text-xl font-medium">
+            {variantPrice.calculatedPrice.formatted()}
+          </p>
+        )}
       </div>
 
       <div>
