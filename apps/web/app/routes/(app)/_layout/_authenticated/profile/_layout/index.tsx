c1b9e6de6236a3e6c200b247edd568ad13fd1bdc
radekskrabucha <rskrabucha13@gmail.com>
2025-03-06 11:35:51 -0500

    chore(web): format

--- apps/web/app/routes/(app)/_layout/_authenticated/profile/_layout/index.tsx
+++ apps/web/app/routes/(app)/_layout/_authenticated/profile/_layout/index.tsx
@@ -2,7 +2,9 @@ import { createFileRoute } from '@tanstack/react-router'
 import { ProfilePage } from '~web/features/profile/ProfilePage'
 import { seo } from '~web/utils/seo'
 
-export const Route = createFileRoute('/(app)/_layout/_authenticated/profile/_layout/')({
+export const Route = createFileRoute(
+  '/(app)/_layout/_authenticated/profile/_layout/'
+)({
   component: ProfilePage,
   head: () => ({
     meta: [...seo({ title: 'Profile' })]
