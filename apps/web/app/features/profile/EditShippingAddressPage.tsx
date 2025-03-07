1a48fe7030514de37f10b496c14e55e9a5fafc8a
radekskrabucha <rskrabucha13@gmail.com>
2025-03-07 11:36:59 -0500

    feat(web/profile): add DeleteAddressButton to EditShippingAddressPage

--- apps/web/app/features/profile/EditShippingAddressPage.tsx
+++ apps/web/app/features/profile/EditShippingAddressPage.tsx
@@ -1,5 +1,15 @@
-export const EditShippingAddressPage = () => (
-  <section className="layout-section gap-8 !self-start">
-    <h2 className="text-2xl font-semibold">Edit Shipping Address</h2>
-  </section>
-)
+import { DeleteAddressButton } from './components/DeleteAddressButton'
+import { editAddressPageRouteApi } from './utils'
+
+export const EditShippingAddressPage = () => {
+  const { id } = editAddressPageRouteApi.useParams()
+
+  return (
+    <section className="layout-section gap-8 !self-start">
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="text-2xl font-semibold">Edit Shipping Address</h2>
+        <DeleteAddressButton id={id} />
+      </div>
+    </section>
+  )
+}
--- apps/web/app/features/profile/utils.ts
+++ apps/web/app/features/profile/utils.ts
@@ -7,3 +7,7 @@ export const resetPasswordPageRouteApi = getRouteApi(
 export const addressDetailsPageRouteApi = getRouteApi(
   '/(app)/_layout/_authenticated/profile/_layout/shipping-address/$id/'
 )
+
+export const editAddressPageRouteApi = getRouteApi(
+  '/(app)/_layout/_authenticated/profile/_layout/shipping-address/$id/edit'
+)
