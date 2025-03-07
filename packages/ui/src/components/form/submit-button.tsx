920304b02ee5112aed10511fb6f40994130c281b
radekskrabucha <rskrabucha13@gmail.com>
2025-03-07 13:13:59 -0500

    feat(ui/components): make SubmitButton text configurable

--- packages/ui/src/components/form/submit-button.tsx
+++ packages/ui/src/components/form/submit-button.tsx
@@ -4,14 +4,18 @@ import { LoadingCircleIndicator } from '../ui/loading-circle-indicator.js'
 
 type SubmitButtonProps = {
   isPending: boolean
+  text: string
 }
 
-export const SubmitButton: React.FC<SubmitButtonProps> = ({ isPending }) => (
+export const SubmitButton: React.FC<SubmitButtonProps> = ({
+  isPending,
+  text
+}) => (
   <Button
     type="submit"
     disabled={isPending}
   >
     {isPending && <LoadingCircleIndicator />}
-    Update Address
+    {text}
   </Button>
 )
