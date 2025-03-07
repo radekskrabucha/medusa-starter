77318bca552fa90495a1376bdfdaffff7e0fde90
radekskrabucha <rskrabucha13@gmail.com>
2025-03-07 14:55:43 -0500

    feat(utils/date): add Unix timestamp utility function

--- packages/utils/src/date.ts
+++ packages/utils/src/date.ts
@@ -1,3 +1,7 @@
+import { getUnixTime } from 'date-fns'
+
 export const getNow = () => new Date()
 
 export const getFullYear = () => getNow().getFullYear()
+
+export const getNowUnix = () => getUnixTime(getNow())
