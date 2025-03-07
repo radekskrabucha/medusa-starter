ec68448c630938a2a86884ffb8c4eeeb25674a85
radekskrabucha <rskrabucha13@gmail.com>
2025-03-07 16:10:24 -0500

    feat(web/config): add external social media links with type-safe configuration

--- apps/web/app/config/app.ts
+++ apps/web/app/config/app.ts
@@ -1,1 +1,15 @@
+import type { StringWithAutoCompleteOptions } from '@medusa-starter/utils/types'
+
 export const appName = 'Medusa Store'
+
+export const ExternalLinks = {
+  Facebook: 'https://www.facebook.com',
+  Instagram: 'https://www.instagram.com',
+  X: 'https://www.x.com'
+} as const
+
+export type ExternalLinks = typeof ExternalLinks
+export type ExternalLink = keyof ExternalLinks
+export type ExternalLinkUrl = StringWithAutoCompleteOptions<
+  ExternalLinks[ExternalLink]
+>
