0058c0eb077abcbdd62dca70ac5775a44c9f22e4
radekskrabucha <rskrabucha13@gmail.com>
2025-03-07 13:04:48 -0500

    refactor(web/auth): simplify useResetPassword hook implementation

--- apps/web/app/features/auth/hooks/useResetPassword.ts
+++ apps/web/app/features/auth/hooks/useResetPassword.ts
@@ -1,24 +1,19 @@
-import type { NoEmpty } from '@medusa-starter/utils/types'
 import { useMutation } from '@tanstack/react-query'
 import { actions } from '~web/lib/medusa'
 
 type UseResetPasswordParams = {
-  email?: string | null
   onSuccess?: (email: string) => void
   onError?: (error: Error) => void
 }
 
 export const useResetPassword = ({
-  email,
   onSuccess,
   onError
 }: UseResetPasswordParams) => {
   return useMutation({
-    mutationFn: email
-      ? () => actions.auth.resetPassword({ identifier: email })
-      : undefined,
-    mutationKey: ['actions.auth.setNewPassword', email],
+    mutationFn: actions.auth.resetPassword,
+    mutationKey: ['actions.auth.resetPassword'],
     onError: onError,
-    onSuccess: () => onSuccess?.(email as NoEmpty<typeof email>)
+    onSuccess: (_, { identifier }) => onSuccess?.(identifier)
   })
 }
