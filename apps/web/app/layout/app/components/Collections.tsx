6c6191e6d9a4b06f872ab8a7f89d7243b77aa855
radekskrabucha <rskrabucha13@gmail.com>
2025-03-07 15:31:31 -0500

    feat(web/layout): add fetching collections in footer

---
+++ apps/web/app/layout/app/components/Collections.tsx
@@ -0,0 +1,33 @@
+import { Link } from '@tanstack/react-router'
+import { appLayoutRouteApi } from '../utils'
+
+export const Collections = () => {
+  const collections = appLayoutRouteApi.useLoaderData()
+
+  if (!collections.length) {
+    return null
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Link
+        to="/collections"
+        className="font-semibold"
+      >
+        Categories
+      </Link>
+      <div className="flex flex-col gap-2">
+        {collections.map(collection => (
+          <Link
+            key={collection.id}
+            to="/collections/$handle"
+            params={{ handle: collection.handle }}
+            className="text-muted-foreground hover:text-foreground text-sm transition-colors"
+          >
+            {collection.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
--- apps/web/app/routes/(app)/_layout.tsx
+++ apps/web/app/routes/(app)/_layout.tsx
@@ -1,6 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { AppLayout } from '~web/layout/app/AppLayout'
+import { actions } from '~web/lib/medusa'
 
 export const Route = createFileRoute('/(app)/_layout')({
-  component: AppLayout
+  component: AppLayout,
+  loader: async () => {
+    try {
+      const res = await actions.store.getCollections({ limit: 5 })
+
+      return res.collections
+    } catch {
+      return []
+    }
+  }
 })
