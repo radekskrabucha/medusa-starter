31c5dc05abdabde62aa1ed9816e6c4f08bd60f62
radekskrabucha <rskrabucha13@gmail.com>
2025-03-09 18:40:39 -0500

    feat(web/profile): implement token refresh and expiration handling in getMeQueryOptions

--- apps/web/app/features/profile/actions.ts
+++ apps/web/app/features/profile/actions.ts
@@ -3,10 +3,22 @@ import type {
   GetAddressParams,
   GetMeCustomerParams
 } from '@medusa-starter/medusa-utils/types'
+import { getNowUnix } from '@medusa-starter/utils/date'
 import { FetchError } from '@medusajs/js-sdk'
 import { queryOptions } from '@tanstack/react-query'
+import {
+  getAuthToken,
+  getAuthTokenTimestamps,
+  removeAuthToken
+} from '~web/features/auth/utils'
 import { actions } from '~web/lib/medusa'
-import { removeAuthToken } from '../auth/utils'
+
+class AuthTokenError extends Error {}
+
+const refreshToken = actions.auth.refreshToken
+
+const TWO_HOURS_IN_SECONDS = 7200
+const THIRTY_MINUTES_IN_SECONDS = 1800
 
 export const getMeQueryOptions = (
   token: string | null,
@@ -16,12 +28,48 @@ export const getMeQueryOptions = (
     queryKey: ['actions.customer.getMe', params, token],
     queryFn: async () => {
       try {
-        return await actions.customer.getMe(params)
+        const token = getAuthToken()
+
+        if (!token) {
+          throw new AuthTokenError('No auth token found')
+        }
+
+        const timestamps = getAuthTokenTimestamps(token)
+
+        if (!timestamps) {
+          throw new AuthTokenError('Invalid auth token')
+        }
+        const nowUnix = getNowUnix()
+
+        if (nowUnix > timestamps.exp) {
+          throw new AuthTokenError('Auth token expired')
+        }
+
+        const difference = timestamps.difference()
+
+        // If the difference is less than two hours and the token will expire in less than 30 minutes, refresh the token
+        if (
+          difference <= TWO_HOURS_IN_SECONDS &&
+          nowUnix - timestamps.exp < THIRTY_MINUTES_IN_SECONDS
+        ) {
+          await refreshToken()
+        }
+        // If the difference is more than two hours, refresh the token if the token was issued more than two hours ago
+        if (nowUnix - timestamps.iat > TWO_HOURS_IN_SECONDS) {
+          await refreshToken()
+        }
+
+        const getMeRes = await actions.customer.getMe(params)
+
+        return getMeRes
       } catch (error) {
         if (error instanceof FetchError) {
           if (error.status === 401) {
             removeAuthToken()
           }
+        }
+        if (error instanceof AuthTokenError) {
+          removeAuthToken()
         }
 
         throw error
