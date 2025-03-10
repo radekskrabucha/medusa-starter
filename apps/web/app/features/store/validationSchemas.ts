b7f9b950f2554f038fba5cc2784db9d07c541b1f
radekskrabucha <rskrabucha13@gmail.com>
2025-03-10 14:34:18 -0500

    feat(web/store): add categories to store search schema

--- apps/web/app/features/store/validationSchemas.ts
+++ apps/web/app/features/store/validationSchemas.ts
@@ -11,10 +11,12 @@ export const collectionsSchema = z.array(z.string())
 
 export const storeSearchSchema = z.object({
   order: sortOrderSchema.optional(),
-  collections: z.array(z.string()).optional()
+  collections: z.array(z.string()).optional(),
+  categories: z.array(z.string()).optional(),
 })
 
 export type StoreSortOrder = z.infer<typeof sortOrderSchema>
 export type StoreSearch = z.infer<typeof storeSearchSchema>
 export type StoreCollections = StoreSearch['collections']
+export type StoreCategories = StoreSearch['categories']
 export type StoreSortOptions = keyof StoreSearch
