84e7575ad41e46e0b3c9a42a856e2c6303c44a17
radekskrabucha <rskrabucha13@gmail.com>
2025-03-10 13:45:14 -0500

    feat(utils): add ArrayElementType utility type

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,3 +1,5 @@
 export type NoEmpty<T> = T extends null | false | undefined ? never : T
 
 export type StringWithAutoCompleteOptions<T extends string> = (string & {}) | T
+
+export type ArrayElementType<T> = T extends (infer U)[] ? U : T
