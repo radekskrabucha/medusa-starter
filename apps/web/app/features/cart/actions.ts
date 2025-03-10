5cd24a1253253e0713e445599e3a2f5f771246b1
radekskrabucha <rskrabucha13@gmail.com>
2025-03-10 18:50:24 -0500

    feat(web/cart): add getCartQueryOptions for cart retrieval

--- apps/web/app/features/cart/actions.ts
+++ apps/web/app/features/cart/actions.ts
@@ -1,4 +1,8 @@
-import type { AddProductToCartParams } from '@medusa-starter/medusa-utils/types'
+import type {
+  AddProductToCartParams,
+  GetCartParams
+} from '@medusa-starter/medusa-utils/types'
+import { queryOptions } from '@tanstack/react-query'
 import { actions } from '~web/lib/medusa'
 import { localCart } from './utils'
 
@@ -26,3 +30,9 @@ export const addProductToCart = async (
     body: req
   })
 }
+
+export const getCartQueryOptions = (params: GetCartParams) =>
+  queryOptions({
+    queryFn: () => actions.cart.get(params),
+    queryKey: ['actions.cart.get', params]
+  })
