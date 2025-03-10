76b585b418a561aa3be704d14ae69be9eeb413f7
radekskrabucha <rskrabucha13@gmail.com>
2025-03-10 18:26:34 -0500

    refactor(web/products): rename product option schema for query context

--- apps/web/app/features/products/validationSchemas.ts
+++ apps/web/app/features/products/validationSchemas.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod'
 
-export const productOptionSchema = z.object({
+export const queryProductOptionSchema = z.object({
   name: z.string(),
   value: z.string()
 })
 
-export type ProductOption = z.infer<typeof productOptionSchema>
+export type QueryProductOption = z.infer<typeof queryProductOptionSchema>
 
 export const productSearchSchema = z.object({
   image: z.string().optional(),
-  options: z.array(productOptionSchema).optional()
+  options: z.array(queryProductOptionSchema).optional()
 })
 
 export type ProductSearch = z.infer<typeof productSearchSchema>
