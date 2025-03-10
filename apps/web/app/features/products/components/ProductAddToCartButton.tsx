56d268e08017be16b8463e21bbdf6e68cbe60f11
radekskrabucha <rskrabucha13@gmail.com>
2025-03-10 18:46:19 -0500

    feat(web/products): implement ProductAddToCartButton with cart mutation

--- apps/web/app/features/products/components/ProductAddToCartButton.tsx
+++ apps/web/app/features/products/components/ProductAddToCartButton.tsx
@@ -1,10 +1,52 @@
+import type { Product } from '@medusa-starter/medusa-utils/models'
 import { Button } from '@medusa-starter/ui/button'
+import { LoadingCircleIndicator } from '@medusa-starter/ui/loading-circle-indicator'
+import { useMutation } from '@tanstack/react-query'
 import { ShoppingCart } from 'lucide-react'
+import type React from 'react'
+import { toast } from 'sonner'
+import {
+  addProductToCart,
+  type AddProductToCartReq
+} from '~web/features/cart/actions'
+import { localCart } from '~web/features/cart/utils'
+import { getProductSelectedVariant, productPageRouteApi } from '../utils'
 
-// TOOD - Add logic for adding product to cart
-export const ProductAddToCartButton = () => {
+type ProductAddToCartButtonProps = {
+  product: Product
+}
+
+export const ProductAddToCartButton: React.FC<ProductAddToCartButtonProps> = ({
+  product
+}) => {
+  const { options } = productPageRouteApi.useSearch()
+  const cartId = localCart.get()
+  const variant = getProductSelectedVariant(product, options)
+
+  const addToCartMutation = useMutation({
+    mutationFn: (res: AddProductToCartReq) => addProductToCart(cartId, res),
+    mutationKey: ['addProductToCart', cartId],
+    onError: error => {
+      toast.error('Failed to add product to cart', {
+        description: error.message
+      })
+    },
+    onSuccess: () => {
+      toast.success('Product added to cart')
+    }
+  })
+
   return (
-    <Button onClick={() => alert('Add to cart')}>
+    <Button
+      onClick={() =>
+        addToCartMutation.mutate({
+          quantity: 1,
+          variant_id: variant.id
+        })
+      }
+      disabled={addToCartMutation.isPending}
+    >
+      {addToCartMutation.isPending && <LoadingCircleIndicator />}
       <ShoppingCart className="size-5" />
       Add to Cart
     </Button>
