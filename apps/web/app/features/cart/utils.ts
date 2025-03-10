f33208cef86fe3ca397a540bd018014681909e42
radekskrabucha <rskrabucha13@gmail.com>
2025-03-10 17:30:06 -0500

    feat(web/cart): add cart event dispatch on local cart changes

--- apps/web/app/features/cart/utils.ts
+++ apps/web/app/features/cart/utils.ts
@@ -1,9 +1,19 @@
 import { getItem, removeItem, setItem } from '~web/utils/localStorage'
 
 export const CART_KEY = 'medusa_cart'
+export const CART_EVENT_NAME = 'cart'
 
 export const localCart = {
   get: () => getItem(CART_KEY),
-  set: (cart: string) => setItem(CART_KEY, cart),
-  remove: () => removeItem(CART_KEY)
+  set: (cart: string) => {
+    setItem(CART_KEY, cart)
+    dispatchCartEvent()
+  },
+  remove: () => {
+    removeItem(CART_KEY)
+    dispatchCartEvent()
+  }
 }
+
+export const dispatchCartEvent = () =>
+  window.dispatchEvent(new Event(CART_EVENT_NAME))
