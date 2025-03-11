322164d38d66c1a3ccdabe9085a1734531a4e1ae
radekskrabucha <rskrabucha13@gmail.com>
2025-03-11 16:27:00 -0500

    feat(web/checkout): add email form to checkout page with validation

--- apps/web/app/features/checkout/CheckoutPage.tsx
+++ apps/web/app/features/checkout/CheckoutPage.tsx
@@ -1,5 +1,28 @@
-export const CheckoutPage = () => (
-  <section className="layout-section">
-    <h3>Checkout Page</h3>
-  </section>
-)
+import { QueryBoundary } from '~web/components/QueryBoundary'
+import { useGetCartQuery } from '../cart/hooks/useGetCartQuery'
+import { EmailForm } from './components/EmailForm'
+
+export const CheckoutPage = () => {
+  const { getCartQuery, cartId } = useGetCartQuery()
+
+  if (!cartId) {
+    throw new Error('No cart id found')
+  }
+  if (getCartQuery.data && !getCartQuery.data.cart.items?.length) {
+    throw new Error('Cart is empty')
+  }
+
+  return (
+    <section className="layout-section gap-8">
+      <h2 className="text-4xl font-semibold">Checkout</h2>
+      <QueryBoundary query={getCartQuery}>
+        {data => (
+          <EmailForm
+            email={data.cart.email}
+            cartId={cartId}
+          />
+        )}
+      </QueryBoundary>
+    </section>
+  )
+}
