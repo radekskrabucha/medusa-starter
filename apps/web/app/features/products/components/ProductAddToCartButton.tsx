cdb95947bb24250bfd987f0f239891d4c84b4cc3
radekskrabucha <rskrabucha13@gmail.com>
2025-03-10 19:40:23 -0500

    feat(web/cart): optimize cart state update after adding product

--- apps/web/app/features/products/components/ProductAddToCartButton.tsx
+++ apps/web/app/features/products/components/ProductAddToCartButton.tsx
@@ -1,12 +1,13 @@
 import type { ProductVariant } from '@medusa-starter/medusa-utils/models'
 import { Button } from '@medusa-starter/ui/button'
 import { LoadingCircleIndicator } from '@medusa-starter/ui/loading-circle-indicator'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { ShoppingCart } from 'lucide-react'
 import type React from 'react'
 import { toast } from 'sonner'
 import {
   addProductToCart,
+  getCartQueryOptions,
   type AddProductToCartReq
 } from '~web/features/cart/actions'
 import { useSyncLocalCart } from '~web/features/cart/hooks/useSyncCartToken'
@@ -19,6 +20,7 @@ export const ProductAddToCartButton: React.FC<ProductAddToCartButtonProps> = ({
   selectedVariant
 }) => {
   const cartId = useSyncLocalCart()
+  const queryClient = useQueryClient()
 
   const addToCartMutation = useMutation({
     mutationFn: (res: AddProductToCartReq) => addProductToCart(cartId, res),
@@ -28,8 +30,13 @@ export const ProductAddToCartButton: React.FC<ProductAddToCartButtonProps> = ({
         description: error.message
       })
     },
-    onSuccess: () => {
+    onSuccess: data => {
       toast.success('Product added to cart')
+
+      queryClient.setQueryData(
+        getCartQueryOptions({ id: cartId ?? '' }).queryKey,
+        data
+      )
     }
   })
 
