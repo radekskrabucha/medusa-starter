dd680feb24fde1580ebd928d5e73fd9391537011
radekskrabucha <rskrabucha13@gmail.com>
2025-03-10 21:46:40 -0500

    feat(web/cart): improve StoreCartCard with scrollable cart items and max height

--- apps/web/app/layout/app/components/StoreCartCard.tsx
+++ apps/web/app/layout/app/components/StoreCartCard.tsx
@@ -22,17 +22,19 @@ export const StoreCartCard: React.FC<StoreCartCardProps> = ({ cart }) => (
   <HoverWrapper numOfItems={calculateNumOfItems(cart)}>
     <Separator />
     {cart.items && cart.items.length > 0 ? (
-      <div className="flex flex-col gap-3">
-        {cart.items.map(item => (
-          <CartItem
-            key={item.id}
-            item={item}
-            currencyCode={cart.currency_code}
-          />
-        ))}
-      </div>
+      <>
+        <div className="scrollbar-hidden flex flex-col gap-3 overflow-y-auto">
+          {cart.items.map(item => (
+            <CartItem
+              key={item.id}
+              item={item}
+              currencyCode={cart.currency_code}
+            />
+          ))}
+        </div>
+        <Separator />
+      </>
     ) : null}
-    <Separator />
     <div className="flex items-center justify-between gap-2">
       <Label>Total</Label>
       <span className="text-muted-foreground text-sm">
@@ -113,7 +115,7 @@ const HoverWrapper: React.FC<HoverWrapperProps> = ({
     </HoverCardTrigger>
     <HoverCardContent
       collisionPadding={16}
-      className="flex min-w-[min(calc(100vw-2rem),400px)] flex-col gap-4"
+      className="flex max-h-[calc(100vh-8rem)] min-w-[min(calc(100vw-2rem),400px)] flex-col gap-4"
     >
       <CartHeader />
       {children}
