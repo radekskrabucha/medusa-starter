aed43030b4ed327ed92b9f379615bab1246e7278
radekskrabucha <rskrabucha13@gmail.com>
2025-03-13 16:29:03 -0500

    feat(checkout): add ShippingOptionsInfo component to display shipping option details and pricing

---
+++ apps/web/app/features/checkout/components/ShippingOptionsInfo.tsx
@@ -0,0 +1,34 @@
+import type { CartShippingOption } from '@medusa-starter/medusa-utils/models'
+import { convertPriceAmountToLocale } from '@medusa-starter/medusa-utils/price'
+
+type ShippingOptionsInfoProps = {
+  shippingOption: CartShippingOption
+}
+
+export const ShippingOptionsInfo: React.FC<ShippingOptionsInfoProps> = ({
+  shippingOption
+}) => {
+  const price =
+    shippingOption.calculated_price?.calculated_amount &&
+    shippingOption.calculated_price?.currency_code
+      ? convertPriceAmountToLocale({
+          amount: shippingOption.calculated_price.calculated_amount,
+          currencyCode: shippingOption.calculated_price.currency_code
+        })
+      : null
+
+  return (
+    <div className="flex flex-1 shrink-0 flex-col gap-2">
+      <h2 className="font-semibold">
+        {shippingOption.name}{' '}
+        {price && (
+          <span className="text-muted-foreground">{price}</span>
+        )}
+      </h2>
+      <div className="text-muted-foreground flex flex-col text-sm">
+        <p>{shippingOption.type.label}</p>
+        <p>{shippingOption.type.description}</p>
+      </div>
+    </div>
+  )
+}
