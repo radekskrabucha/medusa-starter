730531cebca1da28b5b2eea7312740370c70ad02
radekskrabucha <rskrabucha13@gmail.com>
2025-03-14 17:42:22 -0500

    feat(routes): implement order confirmation page with dynamic loading and error handling for order retrieval

--- apps/web/app/routes/(app)/_layout/order.$id.tsx
+++ apps/web/app/routes/(app)/_layout/order.$id.tsx
@@ -1,9 +1,27 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { FetchError } from '@medusajs/js-sdk'
+import { createFileRoute, notFound } from '@tanstack/react-router'
+import { OrderConfirmationPage } from '~web/features/order/OrderConfirmationPage'
+import { actions } from '~web/lib/medusa'
+import { seo } from '~web/utils/seo'
 
 export const Route = createFileRoute('/(app)/_layout/order/$id')({
-  component: RouteComponent
-})
+  component: OrderConfirmationPage,
+  loader: async ({ params }) => {
+    try {
+      const data = await actions.order.getOrder({ id: params.id })
 
-function RouteComponent() {
-  return <div>Hello order $id!</div>
-}
+      return data
+    } catch (error) {
+      if (error instanceof FetchError) {
+        if (error.status === 404) {
+          throw notFound()
+        }
+      }
+
+      throw error
+    }
+  },
+  head: () => ({
+    meta: [...seo({ title: 'Order Confirmed' })]
+  })
+})
