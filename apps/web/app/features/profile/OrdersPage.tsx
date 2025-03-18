6361179b9500d4d88ad8fc2dd903c3b225e9c35e
radekskrabucha <rskrabucha13@gmail.com>
2025-03-18 16:38:19 -0500

    feat(profile): implement orders display in OrdersPage with query handling and OrderCard components

--- apps/web/app/features/profile/OrdersPage.tsx
+++ apps/web/app/features/profile/OrdersPage.tsx
@@ -1,7 +1,30 @@
+import { useQuery } from '@tanstack/react-query'
+import { QueryBoundary } from '~web/components/QueryBoundary'
+import { getOrdersQueryOptions } from './actions'
+import { OrderCard } from './components/OrderCard'
+
 export const OrdersPage = () => {
+  const getOrdersQuery = useQuery(
+    getOrdersQueryOptions({
+      order: '-created_at'
+    })
+  )
+
   return (
     <section className="layout-section gap-8 !self-start">
       <h2 className="text-2xl font-semibold">Orders</h2>
+      <QueryBoundary query={getOrdersQuery}>
+        {({ orders }) => (
+          <div className="flex flex-col gap-4">
+            {orders.map(order => (
+              <OrderCard
+                key={order.id}
+                order={order}
+              />
+            ))}
+          </div>
+        )}
+      </QueryBoundary>
     </section>
   )
 }
