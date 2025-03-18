ec4e8a589d10013a7b6f838aca4a5664ede8d3c7
radekskrabucha <rskrabucha13@gmail.com>
2025-03-17 19:10:04 -0500

    feat(auth): enhance authentication token management by integrating custom storage methods for JWT handling

--- apps/web/app/lib/medusa.ts
+++ apps/web/app/lib/medusa.ts
@@ -1,8 +1,14 @@
+import {
+  getItem,
+  removeItem,
+  setItem
+} from '@medusa-starter/browser-utils/cookie'
 import { getMedusaClientStoreActions } from '@medusa-starter/medusa-utils/actions'
 import Medusa from '@medusajs/js-sdk'
 import { envClient } from '~web/utils/env/client'
+import { getAppNameKebabCase } from '~web/utils/string'
 
-export const AUTH_TOKEN_KEY = 'medusa_auth_token'
+export const AUTH_TOKEN_KEY = `${getAppNameKebabCase()}.medusa_auth_token`
 
 export const sdk = new Medusa({
   baseUrl: envClient.VITE_MEDUSA_BACKEND_URL,
@@ -11,7 +17,13 @@ export const sdk = new Medusa({
   auth: {
     type: 'jwt',
     fetchCredentials: 'include',
-    jwtTokenStorageKey: AUTH_TOKEN_KEY
+    jwtTokenStorageKey: AUTH_TOKEN_KEY,
+    jwtTokenStorageMethod: 'custom',
+    storage: {
+      getItem: key => getItem(key),
+      setItem: (key, value) => setItem(key, value),
+      removeItem: key => removeItem(key)
+    }
   }
 })
 
