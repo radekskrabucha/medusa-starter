d6b111c04bfec6edd7e6c820f84238776b377b50
radekskrabucha <rskrabucha13@gmail.com>
2025-03-18 18:29:45 -0500

    feat(footer): integrate Regions component into Footer for enhanced country selection

--- apps/web/app/layout/app/components/Footer.tsx
+++ apps/web/app/layout/app/components/Footer.tsx
@@ -1,6 +1,7 @@
 import { Link } from '@tanstack/react-router'
 import { SocialLinks } from '~web/components/SocialLinks'
 import { appName } from '~web/config/app'
+import { Regions } from '~web/features/regions/components/Regions'
 import { Categories } from './Categories'
 import { Collections } from './Collections'
 import { Copyright } from './Copyright'
@@ -8,12 +9,15 @@ import { Copyright } from './Copyright'
 export const Footer = () => (
   <footer className="layout-container bg-background">
     <section className="layout-section flex-wrap justify-between gap-10 !py-20 md:!flex-row">
-      <Link
-        to="/"
-        className="text-muted-foreground hover:text-foreground text-xl uppercase transition-colors"
-      >
-        {appName}
-      </Link>
+      <div className="flex flex-col justify-between gap-4">
+        <Link
+          to="/"
+          className="text-muted-foreground hover:text-foreground text-xl uppercase transition-colors"
+        >
+          {appName}
+        </Link>
+        <Regions />
+      </div>
       <div className="flex flex-wrap gap-x-20 gap-y-6 max-sm:gap-x-8 lg:pr-40">
         <Categories />
         <Collections />
