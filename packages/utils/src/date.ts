25f0592d3806833d8a3d8f2dcd65111f04f081d4
radekskrabucha <rskrabucha13@gmail.com>
2025-03-18 16:06:48 -0500

    feat(utils): add formatDateTime utility function for enhanced date formatting

--- packages/utils/src/date.ts
+++ packages/utils/src/date.ts
@@ -8,4 +8,8 @@ export const formatDate = (date: string | Date) => {
   return format(new Date(date), 'MMMM d, yyyy')
 }
 
+export const formatDateTime = (date: string | Date) => {
+  return format(new Date(date), 'MMMM d, yyyy • HH:mm')
+}
+
 export const getNowUnix = () => getUnixTime(getNow())
