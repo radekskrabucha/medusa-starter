e86e2f450b4a435e6a802b0a89ffc79da6f636c3
radekskrabucha <rskrabucha13@gmail.com>
2025-03-18 15:51:14 -0500

    feat(utils): add formatDate utility function for formatting dates

--- packages/utils/src/date.ts
+++ packages/utils/src/date.ts
@@ -1,7 +1,11 @@
-import { getUnixTime } from 'date-fns'
+import { getUnixTime, format } from 'date-fns'
 
 export const getNow = () => new Date()
 
 export const getFullYear = () => getNow().getFullYear()
+
+export const formatDate = (date: string | Date) => {
+  return format(new Date(date), 'MMMM d, yyyy')
+}
 
 export const getNowUnix = () => getUnixTime(getNow())
