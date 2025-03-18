0c208fb1d4cb6ede723988b664a7370d4e558c34
radekskrabucha <rskrabucha13@gmail.com>
2025-03-17 20:02:44 -0500

    refactor(auth): add prefetching of user data in layout loader for improved performance

--- apps/web/app/routes/(app)/_layout.tsx
+++ apps/web/app/routes/(app)/_layout.tsx
@@ -1,10 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { getMeQueryOptions } from '~web/features/profile/actions'
 import { AppLayout } from '~web/layout/app/AppLayout'
 import { actions } from '~web/lib/medusa'
 
 export const Route = createFileRoute('/(app)/_layout')({
   component: AppLayout,
-  loader: async () => {
+  preload: true,
+  loader: async ({ context: { queryClient } }) => {
+    await queryClient.prefetchQuery(getMeQueryOptions())
+
     const [regionsData, collectionsData, categoriesData] = await Promise.all([
       actions.region.getRegions(),
       actions.store.getCollections({ limit: 5 }),
